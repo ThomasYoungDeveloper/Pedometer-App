5533073c705c657ddffc607dba9ccdac71aa1202
Thomas Young <Thomas.young228@gmail.com>
2022-01-09 20:28:33 -0500

    added step counting

--- app/src/main/java/com/thomasyoung/pedometerapp/MainActivity.kt
+++ app/src/main/java/com/thomasyoung/pedometerapp/MainActivity.kt
@@ -7,21 +7,33 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.thomasyoung.pedometerapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
-    private var sensorManager: SensorManager? = null
+    private lateinit var binding: ActivityMainBinding
 
+    private var sensorManager: SensorManager? = null
     private var running = false
     private var totalSteps = 0f
     private var previousTotalSteps = 0f
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+
+        loadData()
+        resetSteps()
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
 
 
@@ -33,15 +45,54 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         val stepSensor = sensorManager?.getDefaultSensor((Sensor.TYPE_STEP_COUNTER))
 
         if (stepSensor == null){
-            Toast.makeText(this, "No Sensor decteded on this device", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "No Sensor detected on this device", Toast.LENGTH_SHORT).show()
+        }else {
+            sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI)
         }
     }
 
-    override fun onSensorChanged(p0: SensorEvent?) {
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
         TODO("Not yet implemented")
     }
 
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-        TODO("Not yet implemented")
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (running){
+            totalSteps = event!!.values[0]
+            val currentSteps = totalSteps.toInt() - previousTotalSteps.toInt()
+            binding.tvStepsTaken.text = ("$currentSteps")
+
+            binding.progressCircular.apply {
+                setProgressWithAnimation(currentSteps.toFloat())
+            }
+        }
+    }
+
+
+    private fun resetSteps() {
+        binding.tvStepsTaken.setOnClickListener{
+            Toast.makeText(this, "Press and Hold to reset", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.tvStepsTaken.setOnLongClickListener{
+            previousTotalSteps = totalSteps
+            binding.tvStepsTaken.text = 0.toString()
+            saveData()
+            true
+        }
+    }
+
+    private fun saveData() {
+        val sharedPreferences= getSharedPreferences("My Preferences", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putFloat("key1", previousTotalSteps)
+        editor.apply()
+    }
+
+    private fun loadData(){
+        val sharedPreferences= getSharedPreferences("My Preferences", Context.MODE_PRIVATE)
+        val savedNumber = sharedPreferences.getFloat("key1", 0f)
+        Log.d("MainActivity", "$savedNumber")
+        previousTotalSteps = savedNumber
+
     }
 }
