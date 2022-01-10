bd43d3a1d16efcce80bdb2e1a64757d69e337eb5
Thomas Young <Thomas.young228@gmail.com>
2022-01-09 19:39:25 -0500

    Initial commit

---
+++ app/src/main/java/com/thomasyoung/pedometerapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.thomasyoung.pedometerapp
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private var sensorManager: SensorManager? = null
+
+    private var running = false
+    private var totalSteps = 0f
+    private var previousTotalSteps = 0f
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        running = true
+        val stepSensor = sensorManager?.getDefaultSensor((Sensor.TYPE_STEP_COUNTER))
+
+        if (stepSensor == null){
+            Toast.makeText(this, "No Sensor decteded on this device", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onSensorChanged(p0: SensorEvent?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        TODO("Not yet implemented")
+    }
+}
